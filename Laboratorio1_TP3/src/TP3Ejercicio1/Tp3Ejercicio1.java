ccdb2ca3b78631573d3ba7cb034d1f00b8bbae29
Gimenez Diego R. I. <141682545+gimenezdiegoruben@users.noreply.github.com>
2025-08-30 21:05:31 -0300

    update Integrantes del grupo 6
    se agregaron los integrantes del grupo 6 en TP1

--- Laboratorio1_TP3/src/TP3Ejercicio1/Tp3Ejercicio1.java
+++ Laboratorio1_TP3/src/TP3Ejercicio1/Tp3Ejercicio1.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package TP3Ejercicio1;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Usuario
+/** * @author GRUPO6 
+ * Luis Ezequiel Sosa, Lucas Saidman, Gimenez Diego Ruben, Carlos German Mecias Giacomelli, Tomas Migliozzi badani, Luca Rodrigaño
  */
 public class Tp3Ejercicio1 extends javax.swing.JFrame {
 
